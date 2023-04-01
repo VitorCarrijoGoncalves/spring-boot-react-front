9aebda1ce91ff648c5f24e4ea2c47f9629261d6c
vitor.goncalves <vitor.goncalves@oabgo.org.br>
2023-03-31 21:31:41 -0300

    Ajustes na tabela para listar os registro da tabela Produtos do banco de dados em tela.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import Formulario from './Formulario';
+import Tabela from './Tabela';
 
 function App() {
+
+  // Objeto produto
+  const produto = {
+    codigo: 0,
+    nome: '',
+    marca: ''
+  }
+
+  // UseState
+  const [btnCadastrar, setBtnCadastrar] = useState(true);
+  const [produtos, setProdutos] = useState([]);
+  const [objProduto, setObjProduto] = useState(produto);
+
+  // Use effect
+  useEffect(()=>{
+    fetch("http://localhost:8090/listar")
+    .then(retorno => retorno.json())
+    .then(retorno_convertido => setProdutos(retorno_convertido));
+  }, []);
+
+  // Retorno  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>{JSON.stringify(objProduto)}</p>
+      <Formulario botao={btnCadastrar} />
+      <Tabela vetor={produtos} /> 
     </div>
   );
 }
