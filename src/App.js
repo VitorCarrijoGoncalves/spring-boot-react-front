1b950817d60793582f72936105376056f1c84bf2
vitor.goncalves <vitor.goncalves@oabgo.org.br>
2023-04-14 22:35:56 -0300

    Criando métodos de Cadastrar e listar produtos

--- src/App.js
+++ src/App.js
@@ -24,16 +24,43 @@ function App() {
     .then(retorno_convertido => setProdutos(retorno_convertido));
   }, []);
 
+  // Limpar formulário
+ // const limparFormulario = () => {
+   // setObjProduto(produto);
+  //}
+
   // Obtendo os dados do formulário
   const aoDigitar = (e) => {
     setObjProduto({...objProduto, [e.target.name]:e.target.value});
   }
 
+  // Cadastrar produto
+  const cadastrar = () => {
+    fetch('http://localhost:8090/cadastrar', {
+      method: 'post',
+      body: JSON.stringify(objProduto),
+      headers: {
+        'Content-type':'application/json',
+        'Accept':'application/json'
+      }
+    })
+    .then(retorno => retorno.json())
+    .then(retorno_convertido => {
+      
+      if (retorno_convertido.mensagem !== undefined) {
+        alert(retorno_convertido.mensagem);
+      } else {
+        setProdutos([...produtos, retorno_convertido]);
+        alert('Produto cadastrado com sucesso!')
+      }
+
+    })
+  }
+
   // Retorno  
   return (
     <div>
-      <p>{JSON.stringify(objProduto)}</p>
-      <Formulario botao={btnCadastrar} eventoTeclado={aoDigitar} />
+      <Formulario botao={btnCadastrar} eventoTeclado={aoDigitar} cadastrar={cadastrar} />
       <Tabela vetor={produtos} /> 
     </div>
   );
